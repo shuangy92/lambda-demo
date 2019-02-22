54c55d15092cf78cad3221ccd931df9ce569b2a0
Shuangy92 <yangshuang1030@gamil.com>
2019-02-22 12:06:50 +0800

    fix typo

--- src/main/java/lambda/demo/StaticGenarator.java
+++ src/main/java/lambda/demo/StaticGenarator.java
@@ -4,11 +4,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class StaticGenarator {
-    private static final int DIVIDER = 10000;
+    private static final int DIVIDEND = 10;
     private static Set<Integer> container;
     static {
         container = new HashSet<>();
-        for (int i = 1; i < Integer.MAX_VALUE / DIVIDER; i++) {
+        for (int i = 1; i < Integer.MAX_VALUE / DIVIDEND; i++) {
             container.add(i % 10000);
         }
     }
